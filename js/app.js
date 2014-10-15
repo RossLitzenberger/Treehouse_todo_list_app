0adcbf693800e2c4486ffe59abad1c17a4d16cf5
RossLitz <codingclasses.049@gmail.com>
2014-10-15 17:45:48 -0500

    Ready to Preform

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//Problem: User Interaction doesn't provide desireed reults.
+//Solution: add interactivty so the user can manage daily tasks.
+
+//Add a new task
+  //When the button is pressed
+  //Create a new list item with the test from #new-task:
+    //input (checkbox)
+    //label
+    //input (text)
+    //button.edit
+    //button.delete
+    //Each elements, need s modified and appended
+
+
+//Edit an exising task
+  //When the Edit button is pressed
+    //if the class of the parent is .editMode
+      //switch from .editMode
+      //label text become the input's value 
+    //esle
+      //Switch to .editMode
+      //input value becomes the label's text
+  
+  //Toggle .editMode
+
+
+//Delete an exising task
+  //When the Delete button is pressed
+    //Remove the parent list item from the ul
+    //
+
+//Mark a task as complete
+  //When the ckeckbox is ckeck
+    //Append the task list item to the #completeed-tasks
+
+
+//Mark a task as incomplete
+  //When the ckeckbox is unckeck
+    //Append the task list item to the #incompleteed-tasks
+
+
+
